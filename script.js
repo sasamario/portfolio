eb97723e9b7a237791f3af2b1fa5248e272f1008
sasamario <43754736+sasamario@users.noreply.github.com>
2019-01-14 23:22:46 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('.slider').slick({
+    dots: true,
+    //slidesToShowは表示するスライドの数を指定できる
+    slidesToShow:2,
+    //autoplayは自動再生、autoplaySpeedは自動再生の速度を設定できる
+    autoplay:true,
+    autoplaySpeed:1700,
+  });
+
+});
