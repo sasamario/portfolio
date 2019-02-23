d77884787b31c01c458926004218c14496bb56e7
sasamario <43754736+sasamario@users.noreply.github.com>
2019-02-23 17:03:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-$(function() {
-	$('.slider').slick({
-    dots: true,
-    //slidesToShowは表示するスライドの数を指定できる
-    slidesToShow:2,
-    //autoplayは自動再生、autoplaySpeedは自動再生の速度を設定できる
-    autoplay:true,
-    autoplaySpeed:1700,
+//スライド
+$(function(){
+  $('.index-btn').click(function(){
+    $('.active').removeClass('active');
+
+    var clickedIndex=$('.index-btn').index($(this));
+
+    $('.slide').eq(clickedIndex).addClass('active');
+    $('.slide-text').eq(clickedIndex).addClass('active');
   });
 
+});
+
+//Q&A
+$(function(){
+	$('.qaa-list-item').click(function(){
+		//クリックされたqaa-list-itemの.answerクラスを変数で指定
+		var $answer=$(this).find('.answer');
+		//その変数がopenクラスを持っていたら削除して解答(.answer)を消す。そして－ボタンを＋ボタンに変更。
+		if($answer.hasClass('open')){
+			$answer.removeClass('open');
+			$answer.slideUp();
+			$(this).find('span').text('+');
+		}else{      //変数がopenクラスを持っていなければopenクラスを追加して解答(.answer)を表示。+ボタンを-ボタンに変更。
+			$answer.addClass('open');
+			$answer.slideDown();
+			$(this).find('span').text('-');
+		}
+	});
 });
